dc21a682cdbb7d785096781cf763bb2eaf0a225b
songning <1457065857@qq.com>
2020-04-26 16:59:23 +0800

    first commit

---
+++ src/main/java/com/uni/config/UniConfigApplication.java
@@ -0,0 +1,13 @@
+package com.uni.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniConfigApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UniConfigApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/uni/config/UniConfigApplicationTests.java
@@ -0,0 +1,16 @@
+package com.uni.config;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class UniConfigApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
