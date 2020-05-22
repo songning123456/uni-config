d5e26451f7562268544323b92e3182f23f7782ff
songning <1457065857@qq.com>
2020-05-22 16:45:47 +0800

    统一配置(待添加 Spring Cloud Bus)

--- src/main/java/com/uni/config/UniConfigApplication.java
+++ src/main/java/com/uni/config/UniConfigApplication.java
@@ -2,8 +2,15 @@ package com.uni.config;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+/**
+ * @author songning
+ */
 @SpringBootApplication
+@EnableEurekaClient
+@EnableConfigServer
 public class UniConfigApplication {
 
     public static void main(String[] args) {
