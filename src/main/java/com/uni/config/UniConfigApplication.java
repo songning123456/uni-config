2bb12d3fa112b6165c681c674cc5777264f12b40
songning123456 <1457065857@qq.com>
2020-05-24 10:25:27 +0800

    优化

--- src/main/java/com/uni/config/UniConfigApplication.java
+++ src/main/java/com/uni/config/UniConfigApplication.java
@@ -2,6 +2,7 @@ package com.uni.config;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
@@ -11,6 +12,7 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 @SpringBootApplication
 @EnableEurekaClient
 @EnableConfigServer
+@EnableDiscoveryClient
 public class UniConfigApplication {
 
     public static void main(String[] args) {
